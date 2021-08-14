e3ea4cdafae996106dded8218a387f76e7b44c07
Fan <52841919+fantastic-y@users.noreply.github.com>
2021-08-13 20:45:46 -0700

    Update activity.js

--- js/activity.js
+++ js/activity.js
@@ -1,28 +1,31 @@
 $(document).ready(function() {
-    $(document).bind("click", function(e) {
-        $(e.target).filter(".tdbox").closest("td").toggleClass("selected");
+    $("#tbody").bind("click", function(e) {
+        $(e.target).closest(".tdbox").toggleClass("tdclick");
+        $("#myModal").modal('show');
     });
 
     //table cells
     $("td").click(function() { //user select a table data cell
         var content = $(this).text(); //get content of cell
-        var cliffName = $("th").eq($(this).index()).text(); //index+eq
+        var cliffName = $(".thead").eq($(this).index()).text(); //index+eq
 
         if (content != "Not Available") { //check if content does not contain a particular string
             $(this).toggleClass("tdhighlight"); //add or remove class when cell is selected
 
             if($(this).hasClass("tdhighlight")) { //check if selected cell has class
-                $('#displaySelected').css("visibility","visible");
-                $('#displaySelected').css("margin-top","2em");
-                $('#result').append("<p>"+content+" at "+cliffName+"</p>"); //add child element with contents of cell
-            } else { //if selected cell dont have class
-                $('#result p:contains('+content+')').remove(); //remove child element
+                $('#myModal').css("visibility","visible");
+                $('#myModal').css("margin-top","2em");
+                $('.modal-body').append("<p>"+content+" at "+cliffName+"</p>"); //add child element with contents of cell
+            } else {
+                $('.modal-body p:contains('+content+')').remove();
+                
+                //if selected cell dont have class
+                /*$('#result p:contains('+content+')').remove(); //remove child element
 
                 if($('#result').has('p').length == false) { //check if there are any child elements within parent
-                    $('#displaySelected').css("visibility", "hidden");
-                    $('#displaySelected').css("margin-top", "0");
+                    $('#myModal').css("visibility", "hidden");
+                    $('#myModal').css("margin-top", "0");*/
                 }
             }
-        }
     });
 });
